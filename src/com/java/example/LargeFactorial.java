aee0321b10e44ec6e6f7c0df50c3f5da78c5f280
Vivek Kumar Gupta <vivgupta1980@gmail.com>
2019-10-21 10:14:14 -0300

    Added large factorial

---
+++ src/com/java/example/LargeFactorial.java
@@ -0,0 +1,19 @@
+package com.java.example;
+
+import java.math.BigInteger;
+
+public class LargeFactorial {
+
+	public static void main(String[] args) {
+		System.out.println("factorial of 18 = " + factorial(18));
+	}
+	
+	public static BigInteger factorial(int num) {
+		BigInteger factorial = BigInteger.ONE;
+		for (int i = num; i > 0; i--) {
+			factorial = factorial.multiply(BigInteger.valueOf(i));
+		}
+		return factorial;
+	}
+
+}
